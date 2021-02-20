ec682f6eb6cea56b8f9f2e0dd5f3b5c9f68bb57f
gj6gj6 <gj6@rice.edu>
2021-02-20 09:25:16 +0800

    header

--- travel_planner_frontend/src/App.js
+++ travel_planner_frontend/src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
-import Header from './components/Header'
+import TravelHeader from './components/Header'
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import PlannerPage from './components/PlannerPage';
 import HomePage from './components/HomePage'
+import Layout from 'antd/lib/layout/layout';
 
 const App = () => (
   <div className="App">
 
 
     <Router>
-      <Header />
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">HomePage</Link>
-            </li>
-            <li>
-              <Link to="/planner">PlannerPage</Link>
-            </li>
-          </ul>
-        </nav>
+        <Layout>
+          <TravelHeader />
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">HomePage</Link>
+              </li>
+              <li>
+                <Link to="/planner">PlannerPage</Link>
+              </li>
+            </ul>
+          </nav>
+        </Layout>
+        
+        
 
         <Switch>
           <Route path="/planner">
--- travel_planner_frontend/src/components/HomePage.js
+++ travel_planner_frontend/src/components/HomePage.js
@@ -1,6 +1,66 @@
 import React, { Component } from "react";
+import { Form, DatePicker, TimePicker, Button } from 'antd';
+
 
 export default class HomePage extends Component {
+//   const { RangePicker } = DatePicker;
+//   const formItemLayout = {
+//   labelCol: {
+//     xs: {
+//       span: 24,
+//     },
+//     sm: {
+//       span: 8,
+//     },
+//   },
+//   wrapperCol: {
+//     xs: {
+//       span: 24,
+//     },
+//     sm: {
+//       span: 16,
+//     },
+//   },
+// };
+// const config = {
+//   rules: [
+//     {
+//       type: 'object',
+//       required: true,
+//       message: 'Please select time!',
+//     },
+//   ],
+// };
+// const rangeConfig = {
+//   rules: [
+//     {
+//       type: 'array',
+//       required: true,
+//       message: 'Please select time!',
+//     },
+//   ],
+// };
+
+// const TimeRelatedForm = () => {
+//   const onFinish = (fieldsValue) => {
+//     // Should format date value before submit.
+//     const rangeValue = fieldsValue['range-picker'];
+//     const rangeTimeValue = fieldsValue['range-time-picker'];
+//     const values = {
+//       ...fieldsValue,
+//       'date-picker': fieldsValue['date-picker'].format('YYYY-MM-DD'),
+//       'date-time-picker': fieldsValue['date-time-picker'].format('YYYY-MM-DD HH:mm:ss'),
+//       'month-picker': fieldsValue['month-picker'].format('YYYY-MM'),
+//       'range-picker': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
+//       'range-time-picker': [
+//         rangeTimeValue[0].format('YYYY-MM-DD HH:mm:ss'),
+//         rangeTimeValue[1].format('YYYY-MM-DD HH:mm:ss'),
+//       ],
+//       'time-picker': fieldsValue['time-picker'].format('HH:mm:ss'),
+//     };
+//     console.log('Received values of form: ', values);
+//   };
+
   render() {
     return <h1>HomePage</h1>;
   }
