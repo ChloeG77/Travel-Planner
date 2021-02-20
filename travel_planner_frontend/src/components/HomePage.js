26215a4969f6f47f0a6e0f94be300ddc6c47d3e8
gj6gj6 <gj6@rice.edu>
2021-02-20 21:51:39 +0800

    some work on home pae

--- travel_planner_frontend/src/components/HomePage.js
+++ travel_planner_frontend/src/components/HomePage.js
@@ -1,67 +1,88 @@
 import React, { Component } from "react";
-import { Form, DatePicker, TimePicker, Button } from 'antd';
+import { Form, DatePicker, Button, Select, Input } from 'antd';
+import { useHistory } from "react-router-dom";
+
+const { RangePicker } = DatePicker;
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 3,
+    },
+  },
+};
+
+const cities = [
+  { label: 'Beijing', value: 'Beijing' },
+  // { label: 'Shanghai', value: 'Shanghai' },
+];
 
 
-export default class HomePage extends Component {
-//   const { RangePicker } = DatePicker;
-//   const formItemLayout = {
-//   labelCol: {
-//     xs: {
-//       span: 24,
-//     },
-//     sm: {
-//       span: 8,
-//     },
-//   },
-//   wrapperCol: {
-//     xs: {
-//       span: 24,
-//     },
-//     sm: {
-//       span: 16,
-//     },
-//   },
-// };
-// const config = {
-//   rules: [
-//     {
-//       type: 'object',
-//       required: true,
-//       message: 'Please select time!',
-//     },
-//   ],
-// };
-// const rangeConfig = {
-//   rules: [
-//     {
-//       type: 'array',
-//       required: true,
-//       message: 'Please select time!',
-//     },
-//   ],
-// };
+const rangeConfig = {
+  rules: [
+    {
+      type: 'array',
+      required: true,
+      message: 'Please select time!',
+    },
+  ],
+};
 
-// const TimeRelatedForm = () => {
-//   const onFinish = (fieldsValue) => {
-//     // Should format date value before submit.
-//     const rangeValue = fieldsValue['range-picker'];
-//     const rangeTimeValue = fieldsValue['range-time-picker'];
-//     const values = {
-//       ...fieldsValue,
-//       'date-picker': fieldsValue['date-picker'].format('YYYY-MM-DD'),
-//       'date-time-picker': fieldsValue['date-time-picker'].format('YYYY-MM-DD HH:mm:ss'),
-//       'month-picker': fieldsValue['month-picker'].format('YYYY-MM'),
-//       'range-picker': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
-//       'range-time-picker': [
-//         rangeTimeValue[0].format('YYYY-MM-DD HH:mm:ss'),
-//         rangeTimeValue[1].format('YYYY-MM-DD HH:mm:ss'),
-//       ],
-//       'time-picker': fieldsValue['time-picker'].format('HH:mm:ss'),
-//     };
-//     console.log('Received values of form: ', values);
-//   };
+const HomePage = () => {
+  const history = useHistory();
 
-  render() {
-    return <h1>HomePage</h1>;
+  const onFinish = (fieldsValue) => {
+    history.push("/planner");
+    // Should format date value before submit.
+    const rangeValue = fieldsValue['traveldate'];
+    console.log(fieldsValue)
+    const values = {
+      ...fieldsValue,
+      'traveldate': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
+    };
+    console.log('Received values of form: ', values);
+  };
+
+ 
+    return (
+      // <h1>HomePage</h1>
+      <Form name="time_related_controls" {...formItemLayout} onFinish={onFinish} initialValues={{
+        destination: ['Beijing']
+      }}>
+        <Form.Item name="tripname" label="Trip Name" rules={[{ required: true, message: 'Please enter a trip name!' }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name="traveldate" label="Travel Date" {...rangeConfig}>
+          <RangePicker />
+        </Form.Item>
+        <Form.Item label="Destination City" name="destination"
+            rules={[{ required: true, message: 'Please select your Desination City!' }]}>
+          <Select options={cities} />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            xs: { span: 24, offset: 0 },
+            sm: { span: 16, offset: 8 },
+          }}
+        > 
+            <Button type="primary" htmlType="submit">
+              Start Planning
+            </Button>
+        </Form.Item>
+      </Form>
+    ) 
   }
-}
+
+
+export default HomePage
