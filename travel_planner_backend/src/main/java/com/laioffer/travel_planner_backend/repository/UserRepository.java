2f8486e6256c17932fb7b6c2b065cf01b9369247
Yuchen Bao <bao.yuc@northeastern.edu>
2021-02-23 16:56:58 -0500

    commit

--- travel_planner_backend/src/main/java/com/laioffer/travel_planner_backend/repository/UserRepository.java
+++ travel_planner_backend/src/main/java/com/laioffer/travel_planner_backend/repository/UserRepository.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface
+UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
