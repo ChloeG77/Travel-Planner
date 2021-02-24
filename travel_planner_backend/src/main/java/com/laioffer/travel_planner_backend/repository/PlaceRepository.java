e88e0825b65237e175d31f2241662d093637389b
Yuchen Bao <bao.yuc@northeastern.edu>
2021-02-23 22:13:06 -0500

    update palce service

---
+++ travel_planner_backend/src/main/java/com/laioffer/travel_planner_backend/repository/PlaceRepository.java
@@ -0,0 +1,8 @@
+package com.laioffer.travel_planner_backend.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaceRepository JpaRepository<Place, Long> {
+
+}
